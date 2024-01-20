b5085127a143768401cc37908651fcddfd412258
peareon <alejandro.peraltad@outlook.com>
2024-01-20 16:34:39 -0600

    minor fixes to experience component

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -25,16 +25,16 @@ export default function Experience(){
                 </div>
                 <div className={`relative lg:h-[125px] sm:h-[92px] md:h-[110px] md:mb-[0px] h-[80px] w-[80%] self-end`}>
                         <div className={`unveil font-Hedvig text-gray-50 text-outline-stroke absolute bg-cover bg-right w-[100%] h-[100%] bg-Alten-bg bg-opacity-10 z-[2] md:text-[20px] text-[17px] flex justify-center items-center font-semibold`}>
-                            QA
+                            HiL
                         </div>
 
                         <div className={`flex justify-evenly items-center relative h-[100%] w-[100%] before:content-[' '] before:block before:absolute 
                         before:left-0 before:top-0 before:w-[100%] before:h-[100%] 
                         before:opacity-[0.2] before:bg-Alten-bg before:bg-cover before:bg-no-repeat before:bg-center`}>
-                            <div className="relative lg:text-[22px] md:text-[20px] text-[14px] font-medium font-Hedvig ">QA</div>
+                            <div className="relative lg:text-[22px] md:text-[20px] text-[14px] font-medium font-Hedvig ">HiL</div>
                             <div className="relative md:w-[55%] w-[60%]">
-                                <div className="bulletpoint lg:text-[14px] md:text-[13px] sm:text-[12px] text-[8.5px] text-start">• Test cases execution and test report generation</div>
-                                <div className="bulletpoint lg:text-[14px] md:text-[13px] sm:text-[12px] text-[8.5px] text-start">• Requirements review and test case creation</div>
+                                <div className="bulletpoint lg:text-[14px] md:text-[13px] sm:text-[12px] text-[8.5px] text-start">• Atomated Test cases execution and test report generation</div>
+                                <div className="bulletpoint lg:text-[14px] md:text-[13px] sm:text-[12px] text-[8.5px] text-start">• Requirements review and creation of automated test cases</div>
                                 <div className="bulletpoint lg:text-[14px] md:text-[13px] sm:text-[12px] text-[8.5px] text-start">• Detailed creation & log of tickets containing bugs</div>
                             </div>
                         </div>
